f9ea2d290115b16f73400f07cceeae122f9c28e4
TasfiaTafannun04 <tafannuntasfia04@gmail.com>
2024-10-14 22:51:35 +0600

    first commit

---
+++ Table.sql
@@ -0,0 +1,16 @@
+CREATE TABLE IF NOT EXISTS Noble_Prize_Winners(
+Year INTEGER,
+Name TEXT,
+Country TEXT,
+Subject TEXT,
+Score TEXT
+);
+
+INSERT INTO Noble_Prize_Winners(Year,Name,Country,Subject,Score)VALUES
+(1956,'Ruby','England','Geography','A*'),
+(1958,'Elise','France','Music','A*'),
+(1961,'John','Florida','Physics','A*'),
+(1966,'Luca','Germany','Math','A'),
+(1999,'Marija','Macedonia','Germany','A');
+
+SELECT * FROM Noble_Prize_Winners WHERE Country NOT LIKE 'F%';
